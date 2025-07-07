3486ad31b26be299ffe1309e7d17d0ee5fb08023
quentin <quentin@dyma.fr>
2025-07-07 15:54:01 +0200

    fix: remove backend

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket       = "mon-tfstate-dyma-bucket-projet1-unique-12345"
-    key          = "projet1-iac/main/terraform.tfstate"
-    region       = "eu-west-3"
-    use_lockfile = true
-    encrypt      = true
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket       = "mon-tfstate-dyma-bucket-projet1-unique-12345"
+#     key          = "projet1-iac/main/terraform.tfstate"
+#     region       = "eu-west-3"
+#     use_lockfile = true
+#     encrypt      = true
+#   }
+# }
